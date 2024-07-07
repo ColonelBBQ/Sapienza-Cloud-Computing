a2c6d4952a8144b0cd430fd06289c6e296df42e7
ColonelBBQ <claaudio.giannini@gmail.com>
2024-07-07 17:28:33 +0200

    Test with session

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,42 @@ import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { fetchAuthSession } from 'aws-amplify/auth';
+import { fetchAuthSession, type AuthSession } from 'aws-amplify/auth';
 
 const client = generateClient<Schema>();
 
-function TodoList({ user, signOut }) {
+function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [session, setSession] = useState<AuthSession | null>(null);
 
   useEffect(() => {
     const refreshSession = async () => {
-      await fetchAuthSession({ forceRefresh: true });
+      try {
+        const newSession = await fetchAuthSession({ forceRefresh: true });
+        setSession(newSession);
+        console.log("id token", newSession.tokens?.idToken);
+        console.log("access token", newSession.tokens?.accessToken);
+      } catch (error) {
+        console.error("Error refreshing session:", error);
+      }
     };
 
     refreshSession();
-  }, [user]);
+  }, []);
 
   useEffect(() => {
     setTodos([]);
-  }, [user]);
+  }, [session]);
 
   useEffect(() => {
-    const subscription = client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    if (session) {
+      const subscription = client.models.Todo.observeQuery().subscribe({
+        next: (data) => setTodos([...data.items]),
+      });
 
-    return () => subscription.unsubscribe();
-  }, [user]);
+      return () => subscription.unsubscribe();
+    }
+  }, [session]);
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -39,33 +49,27 @@ function TodoList({ user, signOut }) {
   }
 
   return (
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-  );
-}
-
-function App() {
-  return (
     <Authenticator>
       {({ signOut, user }) => (
-        <TodoList user={user} signOut={signOut} />
+        <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li 
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>        
       )}
     </Authenticator>
   );
