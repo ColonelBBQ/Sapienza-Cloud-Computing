9222811aef5847f8a403775c6b3006d94e3f1b89
ColonelBBQ <claaudio.giannini@gmail.com>
2024-07-07 16:42:34 +0200

    App change to refresh when signout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { Authenticator } from '@aws-amplify/ui-react'
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { fetchAuthSession } from 'aws-amplify/auth';
 
-await fetchAuthSession({ forceRefresh: true });
-
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  
+  const { user } = useAuthenticator((context) => [context.user]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const refreshSession = async () => {
+      await fetchAuthSession({ forceRefresh: true });
+    };
+
+    refreshSession();
+  }, [user]);
+
+  useEffect(() => {
+    setTodos([]);
+  }, [user]);
+
+  useEffect(() => {
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }, []);
+
+    return () => subscription.unsubscribe();
+  }, [user]);
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -29,7 +41,7 @@ function App() {
 
   return (
     <Authenticator>
-      {({ signOut, user }) => (
+      {({ signOut }) => (
         <main>
           <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <button onClick={createTodo}>+ new</button>
