91461fa433cfd369d4bbd6bd7d1ca9daee5bf9a8
ColonelBBQ <claaudio.giannini@gmail.com>
2024-07-08 21:17:47 +0200

    test 2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
-// import { defineBackend } from "@aws-amplify/backend";
-// import { data } from './data/resource';
-// import { auth } from './auth/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from './data/resource';
+import { auth } from './auth/resource';
 
-// const backend = defineBackend({
-//   data,
-//   auth
-// });
+const backend = defineBackend({
+  data,
+  auth
+});
